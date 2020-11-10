15623a17a209c62bb05851897bafb57b93a4903e
arkaprabhadey <euphoricdey420@gmail.com>
2020-11-10 10:18:11 +0530

    [Arka]Added:Logic for marking attendance according to part and full time

--- employee.js
+++ employee.js
@@ -1,12 +1,37 @@
-const IS_ABSENT=0;
-
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==IS_ABSENT)
+//Method to mark attendance, returns if absent
 {
-    console.log("Absent");
+    const IS_ABSENT=0;
+    let empCheck=Math.floor(Math.random()*10)%2;
+    if(empCheck==IS_ABSENT){
+    console.log("Employee is absent");
     return;
 }
-else if(empCheck==1)
-{
-    console.log("Present");
+else if(empCheck==1){
+    console.log("Employee is present");
 }
+}
+//Method to calculate wage according to part/full-time
+{
+    const IS_PART_TIME=1;
+    const IS_FULL_TIME=2;
+    const PART_TIME_HR=4;
+    const FULL_TIME_HR=8;
+    const WAGER_PER_HOUR=20;
+
+    let empHrs=0;
+    empCheck=Math.floor(Math.random()*10)%3;
+    switch(empCheck){
+        case IS_PART_TIME:
+            empHrs=PART_TIME_HR;
+            break;
+            case IS_FULL_TIME:
+                empHrs=FULL_TIME_HR;
+                break;
+                default:
+                    empHrs=0;
+
+    }
+    let empWage=empHrs*WAGER_PER_HOUR;
+    console.log("Employee Wage is: "+ empWage);
+}
+
